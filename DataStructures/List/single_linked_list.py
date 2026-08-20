5acc22f0409b64f0a4990d25afb6f417c28dcbf1
n.fajardoa11 <n.fajardoa@uniandes.edu.co>
2026-08-20 17:02:30 -0500

    Todo hasta el 13 completo

--- DataStructures/List/single_linked_list.py
+++ DataStructures/List/single_linked_list.py
@@ -6,3 +6,27 @@ def new_list():
     }
 
     return newlist
+
+def get_element(my_list, pos):
+    searchpos = 0
+    node = my_list["first"]
+    while searchpos < pos:
+        node = node["next"]
+        searchpos+= 1
+    return node["info"]
+
+def is_present(my_list, element, cmp_function):
+    is_in_array = False
+    temp= my_list["first"]
+    count = 0
+    
+    while not is_in_array and temp is not None:
+        if cmp_function(element, temp["info"]) ==0:
+            is_in_array = True
+        else:
+            temp = temp["next"]
+            count+=1
+    if not is_in_array:
+        count=-1
+    return count
+
